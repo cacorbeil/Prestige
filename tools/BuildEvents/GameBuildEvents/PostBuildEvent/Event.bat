75ff6d5d8576a13a6cc75fdf9f1ce480a93a01c2
François Vaillancourt <d4rkg1zm0@gmail.com>
2012-01-25 23:45:29 -0500

    Documentation Generation added to build system

--- tools/BuildEvents/GameBuildEvents/PostBuildEvent/Event.bat
+++ tools/BuildEvents/GameBuildEvents/PostBuildEvent/Event.bat
@@ -1,5 +1,12 @@
 echo Custom Post-Build Event Start
 
+set SolutionPath=%~1
+set ProjectPath=%~2
 
+set SharedBuildEventsPath=%SolutionPath%..\Tools\BuildEvents\SharedEvents\
+set DocumentationOutputPath=%SolutionPath%..\Documentation\Game\
+SET DocumentationBatchPath=%SharedBuildEventsPath%Documentation\GenerateDocumentation.bat
+
+CALL %DocumentationBatchPath% %ProjectPath% %DocumentationOutputPath% "Game Template" development
 
 echo Custom Post-Build Event End
---
+++ tools/BuildEvents/SharedEvents/Documentation/GenerateDocumentation.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+SET ProjectDirectory=%~1
+SET ProjectDocumentationOutputDirectory=%~2
+SET ProjectName=%~3
+SET ProjectVersion=%~4
+
+SET DocumentGeneratorDirectory=%ProjectDirectory%..\..\..\thirdparty\doxygen\
+SET DocumentGeneratorExecutable=doxygen.exe
+SET DocumentationGenerationSettingsFilePath=%DocumentGeneratorDirectory%CommonSettings
+
+SET Command=( type %DocumentationGenerationSettingsFilePath% ^
+				^& echo INPUT=%ProjectDirectory% ^
+				^& echo PROJECT_NUMBER=%ProjectVersion% ^
+				^& echo PROJECT_NAME=%ProjectName% ^
+				^& echo OUTPUT_DIRECTORY=%ProjectDocumentationOutputDirectory% ^
+				) ^
+			^| "%DocumentGeneratorDirectory%%DocumentGeneratorExecutable%" -
+
+ECHO Generating Documentation
+%Command%
